7c9d67f1a0919e19d33692295227d7a8f6e80d03
Hari Vishnu Parashar <harryskotch11@gmail.com>
2021-10-06 20:51:48 +0530

    Added image carousel

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,41 +1,51 @@
-import { motion, Transition, Variants } from "framer-motion"
-import React from "react"
-const ani: { variants: Variants, transition: Transition }[] = [
-    {
-        variants: { initial: { opacity: 0 }, animate: { opacity: 1 } },
-        transition: { type: 'spring' },
-    }, {
-        variants: { initial: { opacity: 0, x: 200 }, animate: { opacity: 1, x: 0 } },
-        transition: { type: 'spring', bounce: 100 },
+import { motion, Transition, Variants } from "framer-motion";
+import React from "react";
+const ani: { variants: Variants; transition: Transition }[] = [
+  {
+    variants: { initial: { opacity: 0 }, animate: { opacity: 1 } },
+    transition: { type: "spring" },
+  },
+  {
+    variants: {
+      initial: { opacity: 0, x: 200 },
+      animate: { opacity: 1, x: 0 },
     },
-    {
-        variants: { initial: { opacity: 0, y: -100, x: 200 }, animate: { opacity: 1, y: 0, x: 0 } },
-        transition: { type: 'spring', bounce: 100 },
+    transition: { type: "spring", bounce: 100 },
+  },
+  {
+    variants: {
+      initial: { opacity: 0, y: -100, x: 200 },
+      animate: { opacity: 1, y: 0, x: 0 },
     },
-    {
-        variants: { initial: { opacity: 0, y: -100, x: 200 }, animate: { opacity: 1, y: 0, x: 0 } },
-        transition: { type: 'spring', bounce: 100 },
+    transition: { type: "spring", bounce: 100 },
+  },
+  {
+    variants: {
+      initial: { opacity: 0, y: -100, x: 200 },
+      animate: { opacity: 1, y: 0, x: 0 },
     },
-]
+    transition: { type: "spring", bounce: 100 },
+  },
+];
 export const Animation: React.FunctionComponent<{
-    t: number | undefined;
-    a: number;
+  t: number | undefined;
+  a: number;
 }> = ({ children, t = 400, a }) => {
-    return (
-        <motion.div
-            style={{
-                height: '100%',
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-            initial="initial"
-            animate="animate"
-            variants={ani[a].variants}
-            transition={{ duration: t, ...ani[a].transition }}
-        >
-            {children}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      initial="initial"
+      animate="animate"
+      variants={ani[a].variants}
+      transition={{ duration: t, ...ani[a].transition }}
+    >
+      {children}
+    </motion.div>
+  );
 };
